ef8369e97912aea440dbe1bd478b14429d20e7da
williamadalaqua <wadalaqua@gmail.com>
2026-02-18 10:26:58 -0300

    atividade 1

--- index.js
+++ index.js
@@ -1,51 +1,124 @@
 import express from 'express';
 
 const host = '0.0.0.0';
-const porta = 1750;
+const porta = 9100;
 
 const app = express();
 
-app.get('/', (requisicao, resposta) => {
-    resposta.send(`
-        <!DOCTYPE html>
-        <html lang="pt-br">
-        <head>
-            <meta charset="UTF-8">
-            <title>William - Apresentacao</title>
-        </head>
-        <body>
-            <h1>WILLIAM ALCÂNTARA DALAQUA</h1>
-            <h2>Estudane de T.I</h2>
-                <p><strong>Ola sou estudante de tecnologia, e tenho grande interesse na parte de HTML e CSS, perco horas na frente do computador.</strong></p>
-            <h3>HTML</h3>
-            <h3>CSS</h3>
-            <h3>DESENVOLVEDOR FRONT END</h3>
-            <h4>Telefone: (18) 997653897</h4>
-        </body>
-        </html>
-    `);  
-});
 
-app.get('/horaAtual', (requisicao, resposta) => {
-    const horaAtual = new Date();
-    const hora = horaAtual.getHours() + ':' + horaAtual.getMinutes() + ':' + horaAtual.getSeconds();
-    resposta.send(`
-        <!DOCTYPE html>
+
+app.get('/', (req, res) => {
+
+    const idade = parseInt(req.query.idade);
+    const sexo = req.query.sexo?.toUpperCase();
+    const salarioBase = parseFloat(req.query.salario_base);
+    const anoContratacao = parseInt(req.query.anoContratacao);
+    const matricula = parseInt(req.query.matricula);
+
+    if (isNaN(req.query.idade)) {
+        res.send(`
+             <!DOCTYPE html>
         <html lang="pt-br">
         <head>
             <meta charset="UTF-8">
-            <title>Horario do Servidor</title>
+            <title>Calculadora para reajuste de funcionarios</title>
         </head>
         <body>
-            <h1> Agora são ${hora} </h1>
+            
            
                
+        </body>
+        </html>
+            <h1 style="color:green">FACA OS CALCULOS DE REAJUSTE DE SALARIO !!!</h1>
+            </br>
+            <ul>
+                <p>PASSO A PASSO:</p>
+                <p>VÁ NO LINK DO SEU NAVEGADOR E DIGITE INFORMANDO OS DADOS</p> 
+                <p>IDADE</p>
+                <p>SEXO</p>
+                <P>SALARIO ATUAL</p>
+                <P>EM QUE ANO FOI CONTRATADO NA EMPRESA</p>
+                <p>E A MATRICULA DESSE FUNCIONARIO</p>
+            </ul> 
+            </br>   
+
+            <p>DE ACORDO COM O EXEMPLO ABAIXO:</p>
+
+            <p>http://localhost:9500/?idade=30&sexo=M&salario_base=2000&anoContratacao=2010&matricula=123</p>
+        `);
+    }
+
+  
+        if (
+            isNaN(idade) || idade <= 16 ||
+            (sexo !== "M" && sexo !== "F") ||
+            isNaN(salarioBase) ||
+            isNaN(anoContratacao) || anoContratacao <= 1960 ||
+            isNaN(matricula) || matricula <= 0
+        )   {
+            res.send("<h1>Dados inválidos!</h1>");
+        }
+
+    const anoAtual = new Date().getFullYear();
+    const tempoEmpresa = anoAtual - anoContratacao;
+
+    let percentual = 0;
+    let ajusteFixo = 0;
+
+    
+    if (idade >= 18 && idade <= 39) {
+        if (sexo === "M") {
+            percentual = 0.10;
+            ajusteFixo = tempoEmpresa <= 10 ? -10 : 17;
+        } else {
+            percentual = 0.08;
+            ajusteFixo = tempoEmpresa <= 10 ? -11 : 16;
+        }
+    }
+
+    
+    else if (idade >= 40 && idade <= 69) {
+        if (sexo === "M") {
+            percentual = 0.08;
+            ajusteFixo = tempoEmpresa <= 10 ? -5 : 15;
+        } else {
+            percentual = 0.10;
+            ajusteFixo = tempoEmpresa <= 10 ? -7 : 14;
+        }
+    }
+
+    
+    else if (idade >= 70 && idade <= 99) {
+        if (sexo === "M") {
+            percentual = 0.15;
+            ajusteFixo = tempoEmpresa <= 10 ? -15 : 13;
+        } else {
+            percentual = 0.17;
+            ajusteFixo = tempoEmpresa <= 10 ? -17 : 12;
+        }
+    }
+
+    else {
+        res.write("<h1>Idade fora das faixas permitidas.</h1>");
+    }
+
+    const salarioReajustado = salarioBase + (salarioBase * percentual) + ajusteFixo;
+
+    res.send(`
+        <html>
+        <body>
+            <h1>Resultado do Reajuste</h1>
+            <p>Matricula ${matricula}</p>
+            <p>Idade:: ${idade}</p>
+            <p>Sexo: ${sexo}</p>
+            <p>Salario Base: R$ ${salarioBase}</p>
+            <p>Tempo de Empresa: ${tempoEmpresa} anos</p>
+            <h2> Novo Salario: R$ ${salarioReajustado}</h2>
         </body>
         </html>
     `);
 });
 
-
 app.listen(porta, host, () => {
-  console.log(`Servidor escutando em http://${host}:${porta} `);
+  console.log(`Servidor escutando em http://localhost:${porta} `);
 });
